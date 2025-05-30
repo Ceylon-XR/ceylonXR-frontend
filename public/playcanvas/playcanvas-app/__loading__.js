d2f9055d7be187f8f53d3c2f9133341bf9617302
rvndudz <rvndudz77@gmail.com>
2025-05-30 17:00:32 +0530

    refactor loading screen to improve styling and add title

--- public/playcanvas/playcanvas-app/__loading__.js
+++ public/playcanvas/playcanvas-app/__loading__.js
@@ -1,112 +1,115 @@
 pc.script.createLoadingScreen(function (app) {
-    var showSplash = function () {
-        // splash wrapper
-        var wrapper = document.createElement('div');
-        wrapper.id = 'application-splash-wrapper';
-        document.body.appendChild(wrapper);
+  var showSplash = function () {
+    // splash wrapper
+    var wrapper = document.createElement("div");
+    wrapper.id = "application-splash-wrapper";
+    document.body.appendChild(wrapper);
 
-        // splash
-        var splash = document.createElement('div');
-        splash.id = 'application-splash';
-        wrapper.appendChild(splash);
-        splash.style.display = 'none';
+    // splash
+    var splash = document.createElement("div");
+    splash.id = "application-splash";
+    wrapper.appendChild(splash);
+    splash.style.display = "block";
 
-        var logo = document.createElement('img');
-        logo.src = ASSET_PREFIX + 'logo.png';
-        splash.appendChild(logo);
-        logo.onload = function () {
-            splash.style.display = 'block';
-        };
+    // CeylonXR text title
+    var title = document.createElement("div");
+    title.id = "splash-title";
+    title.innerText = "CeylonXR";
+    splash.appendChild(title);
 
-        var container = document.createElement('div');
-        container.id = 'progress-bar-container';
-        splash.appendChild(container);
+    // progress bar container
+    var container = document.createElement("div");
+    container.id = "progress-bar-container";
+    splash.appendChild(container);
 
-        var bar = document.createElement('div');
-        bar.id = 'progress-bar';
-        container.appendChild(bar);
+    // progress bar
+    var bar = document.createElement("div");
+    bar.id = "progress-bar";
+    container.appendChild(bar);
+  };
 
-    };
+  var hideSplash = function () {
+    var splash = document.getElementById("application-splash-wrapper");
+    splash.parentElement.removeChild(splash);
+  };
 
-    var hideSplash = function () {
-        var splash = document.getElementById('application-splash-wrapper');
-        splash.parentElement.removeChild(splash);
-    };
+  var setProgress = function (value) {
+    var bar = document.getElementById("progress-bar");
+    if (bar) {
+      value = Math.min(1, Math.max(0, value));
+      bar.style.width = value * 100 + "%";
+    }
+  };
 
-    var setProgress = function (value) {
-        var bar = document.getElementById('progress-bar');
-        if (bar) {
-            value = Math.min(1, Math.max(0, value));
-            bar.style.width = value * 100 + '%';
-        }
-    };
+  var createCss = function () {
+    var css = [
+      "body {",
+      "    background-color: #283538;",
+      "    margin: 0;",
+      "    padding: 0;",
+      "    overflow: hidden;",
+      "}",
 
-    var createCss = function () {
-        var css = [
-            'body {',
-            '    background-color: #283538;',
-            '}',
+      "#application-splash-wrapper {",
+      "    position: absolute;",
+      "    top: 0;",
+      "    left: 0;",
+      "    height: 100%;",
+      "    width: 100%;",
+      "    background-color: #283538;",
+      "    display: flex;",
+      "    justify-content: center;",
+      "    align-items: center;",
+      "}",
 
-            '#application-splash-wrapper {',
-            '    position: absolute;',
-            '    top: 0;',
-            '    left: 0;',
-            '    height: 100%;',
-            '    width: 100%;',
-            '    background-color: #283538;',
-            '}',
+      "#application-splash {",
+      "    text-align: center;",
+      "}",
 
-            '#application-splash {',
-            '    position: absolute;',
-            '    top: calc(50% - 28px);',
-            '    width: 264px;',
-            '    left: calc(50% - 132px);',
-            '}',
+      "#splash-title {",
+      "    color: #ffffff;",
+      "    font-family: sans-serif;",
+      "    font-size: 24px;",
+      "    margin-bottom: 20px;",
+      "}",
 
-            '#application-splash img {',
-            '    width: 100%;',
-            '}',
+      "#progress-bar-container {",
+      "    height: 2px;",
+      "    width: 264px;",
+      "    background-color: #1d292c;",
+      "    margin: 0 auto;",
+      "}",
 
-            '#progress-bar-container {',
-            '    margin: 20px auto 0 auto;',
-            '    height: 2px;',
-            '    width: 100%;',
-            '    background-color: #1d292c;',
-            '}',
+      "#progress-bar {",
+      "    width: 0%;",
+      "    height: 100%;",
+      "    background-color: #f60;",
+      "}",
 
-            '#progress-bar {',
-            '    width: 0%;',
-            '    height: 100%;',
-            '    background-color: #f60;',
-            '}',
-            '@media (max-width: 480px) {',
-            '    #application-splash {',
-            '        width: 170px;',
-            '        left: calc(50% - 85px);',
-            '    }',
-            '}'
+      "@media (max-width: 480px) {",
+      "    #progress-bar-container {",
+      "        width: 170px;",
+      "    }",
+      "}",
+    ].join("\n");
 
-        ].join('\n');
+    var style = document.createElement("style");
+    style.type = "text/css";
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
 
-        var style = document.createElement('style');
-        style.type = 'text/css';
-        if (style.styleSheet) {
-            style.styleSheet.cssText = css;
-        } else {
-            style.appendChild(document.createTextNode(css));
-        }
+    document.head.appendChild(style);
+  };
 
-        document.head.appendChild(style);
-    };
+  createCss();
+  showSplash();
 
-
-    createCss();
-
-    showSplash();
-
-    app.on('preload:end', function () {
-        app.off('preload:progress');
-    });
-    app.on('preload:progress', setProgress);
-    app.on('start', hideSplash);
+  app.on("preload:end", function () {
+    app.off("preload:progress");
+  });
+  app.on("preload:progress", setProgress);
+  app.on("start", hideSplash);
 });
