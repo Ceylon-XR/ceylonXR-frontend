159ad54595c711c7b403e80060747d309241ec2d
pasi2222 <pasinduvishmitha2000@gmail.com>
2025-05-30 14:44:35 +0530

    museum

---
+++ public/playcanvas/playcanvas-app/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2250121.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 231874851, 231874875, 231874900, 231874878, 231874861, 231874872, 231874932, 231874935, 231874883, 231874867, 231874880, 231874885, 231874936, 231874877, 231874922, 231874918, 231874941 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/231874904/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/231874899/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/231874903/1/ammo.js', 'preload' : true},
+];
