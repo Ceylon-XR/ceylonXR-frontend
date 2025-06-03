fb440be580571ebba45493218e033a39fef269ee
pasi2222 <pasinduvishmitha2000@gmail.com>
2025-06-04 00:08:31 +0530

    namuro-museum added

---
+++ public/places/namuro-museum/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2254076.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 233029895, 233029949, 233029913, 233029914, 233029899, 233029916, 233029945, 233029897, 233029912, 233029890, 233029915, 233029892, 233029929, 233029964, 233029943, 233029901, 233029898, 233029987, 233029988 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/233029928/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/233029959/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/233029922/1/ammo.js', 'preload' : true},
+];
